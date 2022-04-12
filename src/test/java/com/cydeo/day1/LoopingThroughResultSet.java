b9fbf232d45be6785dbcf254ac1195eb15cc90be
Akbar <akbar@cybertekschool.com>
2022-04-12 14:45:51 -0400

    Looping Through ResultSet and reading cell value

--- src/test/java/com/cydeo/day1/LoopingThroughResultSet.java
+++ src/test/java/com/cydeo/day1/LoopingThroughResultSet.java
@@ -1,5 +1,7 @@
 package com.cydeo.day1;
 
+import java.sql.*;
+
 public class LoopingThroughResultSet {
 
     public static void main(String[] args) {
@@ -8,7 +10,30 @@ public class LoopingThroughResultSet {
         String username = "hr" ;
         String password = "hr" ;
 
+        try {
+            Connection conn = DriverManager.getConnection(url,username,password) ;
+            Statement stmt = conn.createStatement();
+            ResultSet rs   = stmt.executeQuery("SELECT * FROM REGIONS") ;
 
+            // keep looping to next row until there is no more rows when rs.next() return false
+            // rs.next() move the pointer to next row
+            // and return true if there is valid next row and false if there is no valid next row
+            while( rs.next() ){
+
+                System.out.println("rs.getString(\"REGION_NAME\") = "
+                        + rs.getString("REGION_NAME"));
+
+
+            }
+            // where is your pointer right now??-->> AfterLast Location
+            // below line will throw exception because we are at afterlast location
+//            System.out.println("rs.getString(\"REGION_NAME\") = "
+//                    + rs.getString("REGION_NAME"));
+
+
+        } catch (SQLException e) {
+            System.out.println("Exception occurred " + e.getMessage() );
+        }
 
     }
 
