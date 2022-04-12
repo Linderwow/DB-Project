95ea92c8c8fecb91c594b011344963d71ef57c9c
Akbar <akbar@cybertekschool.com>
2022-04-12 16:00:18 -0400

    Class Task Looping backward

--- src/test/java/com/cydeo/day1/LoopingBackward.java
+++ src/test/java/com/cydeo/day1/LoopingBackward.java
@@ -16,6 +16,19 @@ public class LoopingBackward {
         Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
         ResultSet rs   = stmt.executeQuery("SELECT * FROM EMPLOYEES") ;
 
+        // use afterLast() method to go to after last location
+        // use previous() method to move backward
+        // and check whether we have valid previous row or not
+        // stop if there is no more valid row
+        rs.afterLast();
+
+        while( rs.previous() ) {
+
+            System.out.println(rs.getString("EMPLOYEE_ID") + " "
+                                + rs.getString("FIRST_NAME"));
+
+        }
+
 
 
     }
