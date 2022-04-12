1fc0bea4c336c413ddaa6de3ba52438153955810
Akbar <akbar@cybertekschool.com>
2022-04-12 14:28:43 -0400

    Looping through resultset starter

---
+++ src/test/java/com/cydeo/day1/LoopingThroughResultSet.java
@@ -0,0 +1,15 @@
+package com.cydeo.day1;
+
+public class LoopingThroughResultSet {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:oracle:thin:@54.236.150.168:1521:XE" ;
+        String username = "hr" ;
+        String password = "hr" ;
+
+
+
+    }
+
+}
