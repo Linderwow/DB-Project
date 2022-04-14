576dfb7e7b34c1923f963b08a098fed004a2b7cb
Akbar <akbar@cybertekschool.com>
2022-04-14 12:45:10 -0400

    DB_Util practice starter

---
+++ src/test/java/com/cydeo/day2/DB_Util_Practice.java
@@ -0,0 +1,19 @@
+package com.cydeo.day2;
+
+import com.cydeo.utility.DB_Util;
+
+public class DB_Util_Practice {
+
+    public static void main(String[] args) {
+
+
+        String url = "jdbc:oracle:thin:@54.236.150.168:1521:XE" ;
+        String username = "hr" ;
+        String password = "hr" ;
+
+
+
+
+    }
+
+}
