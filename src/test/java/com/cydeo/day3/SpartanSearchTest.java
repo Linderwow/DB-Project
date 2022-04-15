e44646998888a03548e190affdf3c77a47af4f00
Akbar <akbar@cybertekschool.com>
2022-04-15 11:10:24 -0400

    Spartan Search Test starter

---
+++ src/test/java/com/cydeo/day3/SpartanSearchTest.java
@@ -0,0 +1,31 @@
+package com.cydeo.day3;
+
+import org.testng.annotations.Test;
+
+/**
+ * Background : on Spartan UI All Data page ,
+ * Search functionality has been acting up
+ * It does not always return correct result count
+ * You opened the defect , it has been fixed
+ * and yet it occasionally comes back again
+ * Now you are writing automated test to make sure
+ * it works all the times
+ *  few scenario you came up with
+ *    search by gender should work
+ *    search by partial name should work
+ *    search by partial name + gender should
+ *
+ */
+
+public class SpartanSearchTest {
+
+
+    @Test
+    public void testSearchByGender(){
+
+        //
+
+    }
+
+
+}
